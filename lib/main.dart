d62ae3e95e06e1d4c00967f52e73e7d3fc846086
suhwan-cheon <soo7652@naver.com>
2022-05-29 11:20:49 +0900

    feat. make provider class for music list

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:conopot/models/MusicSearchItem.dart';
+import 'package:conopot/models/MusicSearchItemLists.dart';
 import 'package:conopot/models/NavItem.dart';
-import 'package:conopot/models/musicSearchItem.dart';
 import 'package:conopot/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  MusicSearchItemLists().init();
   runApp(const MyApp());
 }
 
@@ -15,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => NavItems(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => NavItems()),
+        ChangeNotifierProvider(create: (context) => MusicSearchItemLists()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'conopot',
--- lib/models/MusicSearchItem.dart
+++ lib/models/MusicSearchItem.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MusicSearchItem {
   final String title;
