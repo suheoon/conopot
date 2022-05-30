1ea2a46c174262298629166ee57b60c79b048cd3
suhwan-cheon <soo7652@naver.com>
2022-05-30 14:56:06 +0900

    feat. develop fitchMeasure page and add fitches

---
+++ lib/models/FitchItem.dart
@@ -0,0 +1,40 @@
+class FitchItem {
+  final int id;
+  final String fitchName;
+  final String fitchCode;
+  final String fitchContext;
+
+  FitchItem(this.id, this.fitchName, this.fitchCode, this.fitchContext);
+}
+
+// sample data
+List<FitchItem> fitchItemList = [
+  FitchItem(1, '1옥타브 도', 'C3', '이 정도는 껌이죠!'),
+  FitchItem(2, '1옥타브 레', 'D3', '이 정도는 껌이죠!'),
+  FitchItem(3, '1옥타브 미', 'E3', '이 정도는 껌이죠!'),
+  FitchItem(4, '1옥타브 파', 'F3', '이 정도는 껌이죠!'),
+  FitchItem(5, '1옥타브 솔', 'G3', '이 정도는 껌이죠!'),
+  FitchItem(6, '1옥타브 라', 'A3', '이 정도는 껌이죠!'),
+  FitchItem(7, '1옥타브 시', 'B3', '이 정도는 껌이죠!'),
+  FitchItem(8, '2옥타브 도', 'C4', '이 정도는 껌이죠!'),
+  FitchItem(9, '2옥타브 레', 'D4', '이 정도는 껌이죠!'),
+  FitchItem(10, '2옥타브 미', 'E4', '이 정도는 껌이죠!'),
+  FitchItem(11, '2옥타브 파', 'F4', '이 정도는 껌이죠!'),
+  FitchItem(12, '2옥타브 솔', 'G4', '이 정도는 껌이죠!'),
+  FitchItem(13, '2옥타브 라', 'A4', '이 정도는 껌이죠!'),
+  FitchItem(14, '2옥타브 시', 'B4', '이 정도는 껌이죠!'),
+  FitchItem(15, '3옥타브 도', 'C5', '이 정도는 껌이죠!'),
+  FitchItem(16, '3옥타브 레', 'D5', '이 정도는 껌이죠!'),
+  FitchItem(17, '3옥타브 미', 'E5', '이 정도는 껌이죠!'),
+  FitchItem(18, '3옥타브 파', 'F5', '이 정도는 껌이죠!'),
+  FitchItem(19, '3옥타브 솔', 'G5', '이 정도는 껌이죠!'),
+  FitchItem(20, '3옥타브 라', 'A5', '이 정도는 껌이죠!'),
+  FitchItem(21, '3옥타브 시', 'B5', '이 정도는 껌이죠!'),
+  FitchItem(22, '4옥타브 도', 'C6', '이 정도는 껌이죠!'),
+  FitchItem(23, '4옥타브 레', 'D6', '이 정도는 껌이죠!'),
+  FitchItem(24, '4옥타브 미', 'E6', '이 정도는 껌이죠!'),
+  FitchItem(25, '4옥타브 파', 'F6', '이 정도는 껌이죠!'),
+  FitchItem(26, '4옥타브 솔', 'G6', '이 정도는 껌이죠!'),
+  FitchItem(27, '4옥타브 라', 'A6', '이 정도는 껌이죠!'),
+  FitchItem(28, '4옥타브 시', 'B6', '이 정도는 껌이죠!'),
+];
--- lib/screens/fitch/fitchMeasure.dart
+++ lib/screens/fitch/fitchMeasure.dart
@@ -1,17 +1,90 @@
+import 'package:conopot/models/FitchItem.dart';
+import 'package:conopot/screens/fitch/components/fitchBanner.dart';
+import 'package:conopot/screens/fitch/components/indicator.dart';
+import 'package:conopot/size_config.dart';
 import 'package:flutter/material.dart';
 
-class FitchMeasure extends StatelessWidget {
-  const FitchMeasure({Key? key}) : super(key: key);
+class FitchMeasure extends StatefulWidget {
+  FitchMeasure({Key? key}) : super(key: key);
 
   @override
+  State<FitchMeasure> createState() => _FitchMeasureState();
+}
+
+class _FitchMeasureState extends State<FitchMeasure> {
+  var _selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('FitchMeasure'),
-      ),
-      body: Center(
-        child: Text('FitchMeasure'),
-      ),
-    );
+        appBar: AppBar(
+          leading: BackButton(color: Colors.black),
+          title: Text(
+            '음역대 측정',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('주의 사항'),
+            Text('blah blah blah ~'),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              height: SizeConfig.screenHeight / 3,
+              child: PageView.builder(
+                onPageChanged: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                controller: PageController(viewportFraction: 0.7),
+                itemCount: fitchItemList.length,
+                itemBuilder: (context, index) {
+                  var fitch = fitchItemList[index];
+                  var _scale = _selectedIndex == index ? 1.0 : 0.8;
+
+                  return TweenAnimationBuilder(
+                    duration: Duration(milliseconds: 350),
+                    tween: Tween(begin: _scale, end: _scale),
+                    curve: Curves.ease,
+                    child: FitchBanner(
+                      fitchItem: fitch,
+                    ),
+                    builder: (context, value, child) {
+                      return Transform.scale(
+                        scale: _scale,
+                        child: child,
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Indicator(
+                  isActive: (_selectedIndex < 7) ? true : false,
+                ),
+                Indicator(
+                  isActive: (7 <= _selectedIndex && _selectedIndex < 7 * 2)
+                      ? true
+                      : false,
+                ),
+                Indicator(
+                  isActive: (7 * 2 <= _selectedIndex && _selectedIndex < 7 * 3)
+                      ? true
+                      : false,
+                ),
+                Indicator(
+                  isActive: (7 * 3 <= _selectedIndex && _selectedIndex < 7 * 4)
+                      ? true
+                      : false,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
