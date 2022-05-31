62e45fb79e6ce521e3e0cad1ebdd258949254a57
suhwan-cheon <soo7652@naver.com>
2022-05-31 12:36:13 +0900

    feat. add flutter_secure_storage(localStorage) for storage users fitch

--- lib/screens/fitch/fitchResult.dart
+++ lib/screens/fitch/fitchResult.dart
@@ -1,11 +1,39 @@
 import 'package:conopot/constants.dart';
+import 'package:conopot/models/MusicSearchItemLists.dart';
 import 'package:conopot/screens/chart/chart_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
 
-class FitchResult extends StatelessWidget {
+class FitchResult extends StatefulWidget {
+  FitchResult({Key? key, required this.pitchLevel}) : super(key: key);
+
   final int pitchLevel;
 
-  const FitchResult({super.key, required this.pitchLevel});
+  @override
+  State<FitchResult> createState() => _FitchResultState(pitchLevel);
+}
+
+class _FitchResultState extends State<FitchResult> {
+  final int pitchLevel;
+
+  _FitchResultState(this.pitchLevel);
+
+  @override
+  void initState() {
+    setUserFitch();
+    super.initState();
+  }
+
+  Future<void> setUserFitch() async {
+    final storage = new FlutterSecureStorage();
+    await storage.write(key: 'userPitch', value: pitchLevel.toString());
+
+    Future.delayed(Duration.zero, () {
+      Provider.of<MusicSearchItemLists>(context, listen: false)
+          .changeUserFitch(pitch: pitchLevel);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
