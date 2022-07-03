81df6cdd500ce44f5852f7f270693863989003e4
suhwan-cheon <soo7652@naver.com>
2022-07-03 13:10:59 +0900

    feat. pitch measure update

--- lib/screens/pitch/pitch_result.dart
+++ lib/screens/pitch/pitch_result.dart
@@ -66,155 +66,36 @@ class _PitchResultState extends State<PitchResult> {
                 height: SizeConfig.defaultSize,
               ),
               Text(
-                '내 최고음 구간',
+                '내 최고음',
                 style: TextStyle(
                   color: kTextColor,
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
-              ),
-              SizedBox(
-                height: SizeConfig.defaultSize * 2,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFE2DCFC),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      pitchNumToString[
-                          (pitchLevel - 2 < 1) ? 1 : pitchLevel - 2],
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.remove),
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFE2DCFC),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      pitchNumToString[pitchLevel],
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
               ),
               SizedBox(
                 height: SizeConfig.defaultSize * 2,
               ),
 
-              Text(
-                '대한민국 평균 음역대',
-                style: TextStyle(
-                  color: kTextColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              Container(
+                padding: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFE2DCFC),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text(
+                  pitchNumToString[pitchLevel],
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
                 ),
               ),
+
               SizedBox(
                 height: SizeConfig.defaultSize * 2,
               ),
-              Text(
-                '남자',
-                style: TextStyle(
-                  color: Color(0xFF0359FF),
-                ),
-              ),
-              SizedBox(
-                height: SizeConfig.defaultSize,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF79A7FF),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      '2옥타브 솔',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.remove),
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF79A7FF),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      '2옥타브 라',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: SizeConfig.defaultSize * 2,
-              ),
-              Text(
-                '여자',
-                style: TextStyle(
-                  color: Color(0xFFFF037C),
-                ),
-              ),
-              SizedBox(
-                height: SizeConfig.defaultSize,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFB83BC),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      '3옥타브 레',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  Icon(Icons.remove),
-                  Container(
-                    padding: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFB83BC),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Text(
-                      '3옥타브 미',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+
               SizedBox(
                 height: SizeConfig.defaultSize * 3,
               ),
@@ -262,6 +143,16 @@ class _PitchResultState extends State<PitchResult> {
             ],
           ),
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Text(
+            '애창곡 노트 설정하러가기',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          icon: Icon(Icons.note_add_outlined),
+          backgroundColor: kPrimaryColor,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
