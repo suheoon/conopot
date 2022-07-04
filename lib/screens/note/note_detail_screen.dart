67def54fb18c5b4a3ca8a8d5970dfde92f1f251b
Suheon Song <ssheon0812@naver.com>
2022-07-04 16:51:43 +0900

    feat: modify detail_screen

--- lib/screens/note/note_detail_screen.dart
+++ lib/screens/note/note_detail_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:pitchupdart/pitch_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../models/note.dart';
@@ -18,36 +19,10 @@ class NoteDetailScreen extends StatefulWidget {
 }
 
 class _NoteDetailScreenState extends State<NoteDetailScreen> {
-  showAlertDialog(BuildContext context) {
-    Widget okButton = ElevatedButton(
-        onPressed: () {
-          Provider.of<NoteData>(context, listen: false).deleteNote(widget.note);
-          Navigator.of(context).popUntil((route) => route.isFirst);
-        },
-        child: Text("노트삭제"));
-
-    Widget cancelButton = ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: Text("취소"));
-
-    AlertDialog alert = AlertDialog(
-      content: Text("노트가 삭제 됩니다."),
-      actions: [okButton, cancelButton],
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
-    List<Note> notes = Provider.of<NoteData>(context, listen: false).notes;
-
+    List<Note> notes = Provider.of<NoteData>(context, listen: true).notes;
+    int index = notes.indexOf(widget.note);
     return Scaffold(
       appBar: AppBar(
         title: Text("곡 상세정보"),
@@ -60,12 +35,13 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
                   color: Colors.red,
                 )),
             onPressed: () {
-              showAlertDialog(context);
+              _showAlertDialog(context);
             },
           ),
         ],
       ),
       body: Column(children: [
+        SizedBox(height: 15),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -134,24 +110,17 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
                     height: 20,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(5)),
-                      color: Colors.grey,
+                      color: Color(0x30826A6A),
                     ),
                     child: Center(child: Text("60398")),
                   ),
                   SizedBox(width: 30),
-                  Text("최고음"),
-                  SizedBox(width: 10),
                   Container(
-                    width: 80,
-                    height: 25,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      color: Color(0xFFF54141),
-                    ),
-                    child: Center(
-                      child: Text("2옥타브 라"),
-                    ),
+                    child: Text("최고음"),
+                    width: 50,
                   ),
+                  SizedBox(width: 10),
+                  _pitchInfo(widget.note.pitch),
                 ],
               ),
               SizedBox(height: 10),
@@ -164,24 +133,36 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
                     height: 20,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(5)),
-                      color: Colors.grey,
+                      color: Color(0x30826A6A),
                     ),
-                    child: Icon(Icons.search),
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Icon(Icons.search)),
                   ),
                   SizedBox(width: 30),
-                  Text("키조정"),
-                  SizedBox(width: 10),
                   Container(
-                    width: 80,
-                    height: 25,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      color: Color(0xFFF54141),
-                    ),
-                    child: Center(
-                      child: Text("2옥타브 라"),
-                    ),
+                    child: Text("키조정"),
+                    width: 50,
                   ),
+                  Row(children: [
+                    IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: () {
+                        Provider.of<NoteData>(context, listen: false)
+                            .minuskeyAdjustment(index);
+                      },
+                    ),
+                    SizedBox(width: 5),
+                    Text('${notes[index].keyAdjustment}'),
+                    SizedBox(width: 5),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        Provider.of<NoteData>(context, listen: false)
+                            .pluskeyAdjustment(index);
+                      },
+                    )
+                  ])
                 ],
               ),
               SizedBox(height: 30),
@@ -204,11 +185,118 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
                 ],
               ),
               SizedBox(height: 30),
-              Text("노래 추천"),
+              _recommendList(widget.note.pitch)
             ],
           ),
         ),
       ]),
     );
+  }
+
+  // 정보요청 다이어로그 창
+  _showRequestDialog(BuildContext context) {
+    Widget requestButton = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // 정보요청
+            Navigator.of(context).pop();
+          },
+          child: Text("정보 요청"),
+        ),
+      ],
+    );
+
+    AlertDialog alert = AlertDialog(
+      content: Text(
+        "최고음이 표시 되지 않을 경우 정보를 요청해주세요 ☺️",
+        style: TextStyle(fontSize: 11),
+      ),
+      actions: [requestButton],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(child: alert);
+        });
+  }
+
+  _showAlertDialog(BuildContext context) {
+    Widget okButton = ElevatedButton(
+        onPressed: () {
+          Provider.of<NoteData>(context, listen: false).deleteNote(widget.note);
+          Navigator.of(context).popUntil((route) => route.isFirst);
+        },
+        child: Text("삭제"));
+
+    Widget cancelButton = ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Text("취소"));
+
+    AlertDialog alert = AlertDialog(
+      content: Text("노트가 삭제 됩니다."),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [okButton, SizedBox(width: 10,),cancelButton],
+        ),
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(child: alert);
+        });
+  }
+
+  Widget _pitchInfo(String pitch) {
+    print(pitch);
+    return pitch == '?'
+        ? GestureDetector(
+            onTap: () {
+              _showRequestDialog(context);
+            },
+            child: Container(
+              width: 80,
+              height: 25,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                color: Color(0xFF7F8A8E),
+              ),
+              child: Center(
+                child: Text(
+                  "정보요청",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        : Container(
+            width: 80,
+            height: 25,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              color: Color(0xFFF54141),
+            ),
+            child: Center(
+              child: Text(pitch),
+            ),
+          );
+  }
+
+  Widget _recommendList(String pitch) {
+    return pitch == '?'
+        ? Container()
+        : Column(
+            children: [
+              Text("노래 추천"),
+              SizedBox(height: 20),
+            ],
+          );
   }
 }
