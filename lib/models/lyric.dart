b47532d7b7b6c049f2374ef0b0b96f214e708021
suhwan-cheon <soo7652@naver.com>
2022-08-07 14:17:32 +0900

    refactor: version update for test(event, resource update)

--- lib/models/lyric.dart
+++ lib/models/lyric.dart
@@ -7,7 +7,7 @@ class Lyric {
     if (json.length == 0 || json[0].length == 0 || json[0][0].length == 0) {
       return Lyric(
           lyric:
-              "해당 노래에 대한 가사 정보가 없습니다\n가사 요청은\n내 정보 페이지 하단의 문의하기를 이용해주세요 🙋‍♂️");
+              "해당 노래에 대한 가사 정보가 없습니다\n가사 요청은\n설정 페이지 하단의 문의하기를 이용해주세요 🙋‍♂️");
     } else {
       return Lyric(
         lyric: json[0][0]['lyrics'],
