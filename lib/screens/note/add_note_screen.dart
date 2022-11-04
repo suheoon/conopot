e8506ffd03f92b67612af6831be0ff5701395ce2
suhwan-cheon <soo7652@naver.com>
2022-11-04 09:52:21 +0900

    fix: 노트 추가 뷰  이벤트 추가

--- lib/screens/note/add_note_screen.dart
+++ lib/screens/note/add_note_screen.dart
@@ -1,3 +1,4 @@
+import 'package:conopot/config/analytics_config.dart';
 import 'package:conopot/models/music_search_item_list.dart';
 import 'package:conopot/screens/note/components/note_search_list.dart';
 import 'package:flutter/material.dart';
@@ -14,11 +15,19 @@ class AddNoteScreen extends StatefulWidget {
 
 class _AddNoteScreenState extends State<AddNoteScreen> {
   @override
+  void initState() {
+    Analytics_config().addNotePageView();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     ToastContext().init(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("노래 추가", style: TextStyle(fontWeight: FontWeight.w700)),
+        title:
+            const Text("노래 추가", style: TextStyle(fontWeight: FontWeight.w700)),
         centerTitle: true,
       ),
       body: Consumer<MusicSearchItemLists>(
