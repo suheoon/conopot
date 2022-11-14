0ffdb9d33c02ea53cbbc5e72fe8a826b1f686804
Suheon Song <ssheon0812@naver.com>
2022-11-14 13:44:14 +0900

    feat: 튜토리얼 페이지뷰 추가

--- lib/tutorial_screen.dart
+++ lib/tutorial_screen.dart
@@ -1,58 +1,136 @@
+import 'package:conopot/config/constants.dart';
 import 'package:conopot/config/size_config.dart';
-import 'package:conopot/tutorial_page.dart';
+import 'package:conopot/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class TutorialScreen extends StatelessWidget {
-  TutorialScreen({Key? key}) : super(key: key);
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({Key? key}) : super(key: key);
 
-  PageController controller = PageController(initialPage: 0);
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  PageController controller =
+      PageController(initialPage: 0, viewportFraction: 1.0);
+  int selectedIndex = 0;
+  double defaultSize = SizeConfig.defaultSize;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(
-      controller: controller,
-      // physics: NeverScrollableScrollPhysics(),
-      children: [
-        TutorialPage(
-          image: SvgPicture.asset(
-            "assets/icons/tutorial1.svg",
+        body: SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: defaultSize * 2),
+          Container(
+              margin: EdgeInsets.only(right: defaultSize * 1.5),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: _buildPageIndicator())),
+          SizedBox(height: defaultSize * 3),
+          Expanded(
+            child: PageView(
+              controller: controller,
+              onPageChanged: (int page) {
+                setState(() {
+                  selectedIndex = page;
+                });
+              },
+              children: [
+                Image.asset(
+                  "assets/images/tutorial1.png",
+                ),
+                Image.asset(
+                  "assets/images/tutorial2.png",
+                ),
+                Image.asset(
+                  "assets/images/tutorial3.png",
+                ),
+                Image.asset(
+                  "assets/images/tutorial4.png",
+                ),
+              ],
+            ),
           ),
-          noOfScreen: 4,
-          onNextPressed: changeScreen,
-          currentScreenNo: 0,
-        ),
-        TutorialPage(
-          image: SvgPicture.asset(
-            "assets/icons/tutorial2.svg",
-          ),
-          noOfScreen: 4,
-          onNextPressed: changeScreen,
-          currentScreenNo: 1,
-        ),
-        TutorialPage(
-          image: SvgPicture.asset(
-            "assets/icons/tutorial3.svg",
-          ),
-          noOfScreen: 4,
-          onNextPressed: changeScreen,
-          currentScreenNo: 2,
-        ),
-        TutorialPage(
-          image: SvgPicture.asset(
-            "assets/icons/tutorial4.svg",
-          ),
-          noOfScreen: 4,
-          onNextPressed: changeScreen,
-          currentScreenNo: 3,
-        ),
-      ],
+          (selectedIndex == 3)
+              ? GestureDetector(
+                  onTap: () {
+                    //"시작하기" 버튼 누를 시, 다음부터는 튜토리얼 화면이 뜨지 않는다.
+                    // await storage.write(key: 'tutorial', value: '1');
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => MainScreen()));
+                  },
+                  child: Container(
+                    width: SizeConfig.screenWidth,
+                    padding: EdgeInsets.all(defaultSize * 1.5),
+                    margin: EdgeInsets.symmetric(horizontal: defaultSize * 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: kMainColor),
+                    child: Center(
+                      child: Text(
+                        '시작하기',
+                        style: TextStyle(
+                            color: kPrimaryWhiteColor,
+                            fontWeight: FontWeight.w600,
+                            fontSize: defaultSize * 1.5),
+                      ),
+                    ),
+                  ))
+              : GestureDetector(
+                  onTap: () {
+                    controller.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease);
+                  },
+                  child: Container(
+                    width: SizeConfig.screenWidth,
+                    padding: EdgeInsets.all(defaultSize * 1.5),
+                    margin: EdgeInsets.symmetric(horizontal: defaultSize * 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: kMainColor),
+                    child: Center(
+                      child: Text(
+                        '다음',
+                        style: TextStyle(
+                            color: kPrimaryWhiteColor,
+                            fontWeight: FontWeight.w600,
+                            fontSize: defaultSize * 1.5),
+                      ),
+                    ),
+                  ))
+        ],
+      ),
     ));
   }
 
   changeScreen(int nextScreenNo) {
     controller.animateToPage(nextScreenNo,
         duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  Widget _indicator(bool isActiveScreen) {
+    return Container(
+      height: defaultSize,
+      child: AnimatedContainer(
+        duration: Duration(microseconds: 150),
+        margin: EdgeInsets.symmetric(horizontal: defaultSize * 0.4),
+        height: defaultSize * 0.8,
+        width: defaultSize * 0.8,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: kMainColor.withOpacity(isActiveScreen ? 1 : 0.4)),
+      ),
+    );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < 4; i++) {
+      list.add(i == selectedIndex ? _indicator(true) : _indicator(false));
+    }
+    return list;
   }
 }
